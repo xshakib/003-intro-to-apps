38c2575cf400cfb6e5012b32f7fcd2ba5a0ced61
Your Name <you@example.com>
2022-07-12 22:54:33 +0000

    updated barchart

--- app.py
+++ app.py
@@ -1,50 +1,49 @@
 ######### import libraries 
-
 import dash
 from dash import html
 from dash import dcc
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
+shows=['Breaking Bad', 'The Boys', 'The Office', 'That 70s Show']
+Comedy_values=[30.5, 10.4, 99, 70]
+Drama_values=[99, 80, 40, 70]
+color1='blue'
+color2='darkred'
+mytitle='Show Comparison'
 
-label1='IBU'
-label2='ABV'
+label1='Comedy'
+label2='Drama'
 
 ########### Set up the chart
 
-def make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle):
-    bitterness = go.Bar(
-        x=beers,
-        y=ibu_values,
+def make_that_cool_barchart(shows, Comedy_values, Drama_values, color1, color2, mytitle):
+    funny = go.Bar(
+        x=shows,
+        y=Comedy_values,
         name=label1,
         marker={'color':color1}
     )
-    alcohol = go.Bar(
-        x=beers,
-        y=abv_values,
+    seriousness = go.Bar(
+        x=shows,
+        y=Drama_values,
         name=label2,
         marker={'color':color2}
     )
 
-    beer_data = [bitterness, alcohol]
-    beer_layout = go.Layout(
+    show_data = [funny, seriousness]
+    show_layout = go.Layout(
         barmode='group',
         title = mytitle
     )
 
-    beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-    return beer_fig
+    show_fig = go.Figure(data=show_data, layout=show_layout)
+    return show_fig
 
 
 ######### Run the function #######
 
 if __name__ == '__main__':
-    fig = make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle)
+    fig = make_that_cool_barchart(shows, Comedy_values, Drama_values, color1, color2, mytitle)
     fig.write_html('docs/barchart.html')
     print('We successfully updated the barchart!')
